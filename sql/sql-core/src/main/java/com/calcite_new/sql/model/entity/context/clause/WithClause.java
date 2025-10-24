b9ef0a38dac636b6d426fcbfd4c94c37555a92bc
TruvisWork <anishkumar@truvis.ai>
2025-10-24 14:33:58 +0530

    Delete sql directory

--- sql/sql-core/src/main/java/com/calcite_new/sql/model/entity/context/clause/WithClause.java
+++
@@ -1,4 +0,0 @@
-package com.calcite_new.sql.model.entity.context.clause;
-
-public class WithClause {
-}
--- sql/sql-core/src/main/java/com/calcite_new/sql/relationextractor/RelationshipType.java
+++
@@ -1,6 +0,0 @@
-package com.calcite_new.sql.relationextractor;
-
-public enum RelationshipType {
-    DEPENDS_ON,
-    ACCESSES
-}
