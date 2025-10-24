0e4b8bf479f1958a66dd4b4f7b0afd21fc4377ef
TruvisWork <anishkumar@truvis.ai>
2025-10-24 14:34:25 +0530

    Delete java/sql directory

--- java/sql/core/core/processor/visitor/StatementVisitor.java
+++
@@ -1,7 +0,0 @@
-package com.calcite_new.sql.core.processor.visitor;
-
-import org.apache.calcite.sql.SqlCall;
-
-public interface StatementVisitor {
-    SqlNodeVisitor.Result visit(SqlCall call);
-}
--- java/sql/core/relationextractor/EntityType.java
+++
@@ -1,9 +0,0 @@
-package com.calcite_new.sql.relationextractor;
-
-public enum EntityType {
-    TABLE,
-    VIEW,
-    EXTERNAL_TABLE,
-    FILE,
-    USER
-}
